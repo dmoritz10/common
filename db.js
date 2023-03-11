a7c82554ebb772b94f0a79f7e60f20d29719304a
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-03-10 17:04:05 -0800

    dan

--- db.js
+++ db.js
@@ -943,7 +943,7 @@ console.log('begin', ts(new Date()))
   oma.setMinutes(oma.getMinutes() - 1);
 
   console.log('writeThrottle', wtArr.map( ele => ts(ele)))
-  console.log('oma', ts(oma), ts(wtArr[i]), wtArr[i] < oma)
+  // console.log('oma', ts(oma), ts(wtArr[i]), wtArr[i] < oma)
 
   for (let i=wtArr.length-1; i>=0; i--) {
     if (wtArr[i] < oma) {console.log('remove wtArr', i, ts(wtArr[i]), ts(oma));wtArr.splice(i, 1)}
