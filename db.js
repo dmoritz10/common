5409b18151adfa267da6b6da5924396070c4c12b
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2023-03-28 14:46:42 -0700

    dan

--- db.js
+++ db.js
@@ -1,4 +1,52 @@
+const Retrier = class {
+  constructor(opts = {}) {
+      this.opts = {};
+      this.attempt = 0;
+      this.opts.limit = opts.limit || 1;
+      this.opts.delay = opts.delay || 0;
+      this.opts.firstAttemptDelay = opts.firstAttemptDelay || 0;
+      this.opts.reAuth = opts.reAuth || [401, 403];
+      this.opts.quotaExceeded = opts.quotaExceeded;
+  }
+  resolve(fn) {
+      this.fn = fn;
+      return new Promise((resolve, reject) => {
+          this._resolve = resolve;
+          this._reject = reject;
+          this.attempt = 0;
+          this._doRetry();
+      });
+  }
+  _doRetry(recentError) {
+      if (this.attempt >= this.opts.limit) {
+          return this._reject(recentError || new Error('Retry limit reached!'));
+      }
+      setTimeout(() => {
+          const promise = this.fn(this.attempt);
 
+          if (!(promise instanceof Promise)) {
+              // TODO: throw error in contructor if params aren't valid
+              return this._reject(new Error('Expecting function which returns promise!'));
+          }
+          promise.then(response => {
+              console.log('then', response, this.attempt)
+
+              this._resolve(response);
+          }, async error => {
+              console.log('error', error, this.attempt)
+
+              if (this.opts.reAuth.indexOf(error.status) > -1) {
+                  await Goth.token()              // for authorization errors obtain an access token
+                  this._doRetry(error);
+              }
+              else if (this.opts.quotaExceeded.indexOf(error.status) > -1) {
+                  this.attempt++;
+                  this._doRetry(error);
+              }
+          });
+      }, this.attempt === 0 ? this.opts.firstAttemptDelay : this.opts.delay);
+  }
+}
 //  database access
 
 //  Sheets
