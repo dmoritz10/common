fd83083e49b2115b64a232562208898d70b6b1f4
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2024-09-27 06:04:04 -0700

    dan

--- gapi.js
+++ gapi.js
@@ -1060,3 +1060,24 @@ const Retrier = class {
     return response 
 
   }
+
+  async function listAlbums() {
+
+    const callerName = new Error().stack.split(/\r\n|\r|\n/g)[1].trim().split(" ")[1]
+    console.log('pre gapi', callerName)     
+
+    const options = { limit: 5, delay: 2000, quotaExceeded: [429, 403]};
+    const retrier = new Retrier(options);
+    let response = await retrier
+      .resolve(async attempt => await gapi.client.photoslibrary.albums.list()
+    )
+      .then(
+          result => {console.log('result', result);return result},
+          error =>  {console.log(error) ;return error}
+      );
+    
+    console.log('post gapi', callerName)  
+                    
+    return response 
+
+  }
